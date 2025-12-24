ad9c2443d791ca1de105511abf22f1247bba19eb
Rohit0035 <75827103+Rohit0035@users.noreply.github.com>
2025-12-24 17:24:11 +0530

    new changes

--- src/components/MainHeroSlider.js
+++ src/components/MainHeroSlider.js
@@ -10,6 +10,8 @@ import { Link } from "react-router-dom";
 import { IMAGE_URL } from "../utils/api-config";
 
 const MainHeroSlider = ({ banners = [] }) => {
+
+
   return (
     <section className="main-slider-wrapper position-relative">
       <Swiper
@@ -23,7 +25,7 @@ const MainHeroSlider = ({ banners = [] }) => {
         {banners.length > 0 &&
           banners.map((banner, index) =>
             <SwiperSlide key={index}>
-              <div className="slider-content position-absolute top-50 start-0 end-0 translate-middle-y">
+              <div className="slider-content position-absolute top-50 start-0 end-0 translate-middle-y" >
                 <div className="container px-3">
                   <p className="sub-title mb-0">
                     {banner.title}
@@ -31,26 +33,28 @@ const MainHeroSlider = ({ banners = [] }) => {
                   <h2 className="large-title">
                     {banner.description}
                   </h2>
-                  <div className="slide-btn mt-lg-3">
+                  {/* <div className="slide-btn mt-lg-3">
                     <Link
                       to="/shop"
                       className="btn btn-dark px-md-5 py-md-3 rounded-5"
                     >
                       Explore Collection
                     </Link>
-                  </div>
+                  </div> */}
                 </div>
               </div>
-              <img
-                src={`${IMAGE_URL}/${banner.image}`}
-                className="img-fluid rounded-0 d-none d-sm-block"
-                alt="Slide 1"
-              />
-              <img
-                src={`${IMAGE_URL}/${banner.mobile_image}`}
-                className="img-fluid rounded-0 d-block d-sm-none"
-                alt="Slide 1"
-              />
+              <Link to="/shop">
+                <img
+                  src={`${IMAGE_URL}/${banner.image}`}
+                  className="img-fluid rounded-0 d-none d-sm-block"
+                  alt="Slide 1"
+                />
+                <img
+                  src={`${IMAGE_URL}/${banner.mobile_image}`}
+                  className="img-fluid rounded-0 d-block d-sm-none"
+                  alt="Slide 1"
+                />
+              </Link>
             </SwiperSlide>
           )}
       </Swiper>
